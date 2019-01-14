7df0b583b00cd66b6c011a900f8411b36231ed7b
bmq92 <bmq928@gmail.com>
2019-01-14 13:00:27 +0700

    new project template

--- src/project/controller.js
+++ src/project/controller.js
@@ -2,20 +2,28 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 const config = require('config')
+const {ncp} = require('ncp')
 
 const mkdir = util.promisify(fs.mkdir)
 const exists = util.promisify(fs.exists)
+const copyFolder = util.promisify(ncp)
 
 const PROJECT_STORAGE = config.get('project-storage')
 
-
+/**
+ * create new project
+ * @param {String} name name of the project
+ */
 module.exports.newProject = async (name) => {
-  if(!name) throw new AppError('name is required')
+  if (!name) throw new AppError('name is required')
 
   const projectPath = path.join(PROJECT_STORAGE, name)
-  if(await exists(projectPath)) throw new AppError('project is existen')
+  const templatePath = path.join(__dirname, 'templates')
+  
+  if (await exists(projectPath)) throw new AppError('project is existen')
 
   await mkdir(projectPath)
+  await copyFolder(templatePath, projectPath) // write template to new project
 
   return 'done'
 }
