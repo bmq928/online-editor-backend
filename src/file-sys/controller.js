e7cf2f978c795888f2d83f2087abb148ad0bf359
bmq928 <bmq928@gmail.com>
2019-04-02 15:23:55 +0700

    complete file-sys

--- src/file-sys/controller.js
+++ src/file-sys/controller.js
@@ -1,17 +1,51 @@
+const util = require('util')
+const path = require('path')
+const rimraf = require('rimraf')
+const config = require('config')
+const fs = require('fs')
+
+const { AppError } = require('../app-error')
+const PROJECT_STORAGE = config.get('project-storage')
+
+
+const removeItem = util.promisify(rimraf)
+const exists = util.promisify(fs.exists)
+const createFile = util.promisify(fs.writeFile)
+const mkdir = util.promisify(fs.mkdir)
 
 /**
  * @param {String} project projectName
  * @param {String} fileName fileName
  */
 module.exports.deleteFile = async (project, fileName) => {
+  if (!project) throw new AppError('project is required')
+  if (!fileName) throw new AppError('file name is required')
 
+  const filePath = path.join(PROJECT_STORAGE, project, fileName)
+  if (!(await exists(filePath))) throw new AppError('file is not exist')
+
+  await removeItem(filePath)
+
+  return 'done'
 }
 
 /**
  * @param {String} project projectName
- * @param {String} fileName fileName
+ * @param {String} file fileName or a path to file e.g /folder/nah.py
  */
-module.exports.newFile = async (project, fileName) => {
+module.exports.newFile = async (project, file) => {
+  if (!project) throw new AppError('project is required')
+  if (!file) throw new AppError('file name is required')
+
+  const filePath = path.join(PROJECT_STORAGE, project, file)
+  const parentFolder = path.dirname(filePath)
+  
+
+  if (!(await exists(parentFolder))) throw new AppError('parent folder is not exist')
+  if (await exists(filePath)) throw new AppError('file is exist')
+
+  await createFile(filePath, '', { encoding: 'utf8' })
+  return 'done'
 
 }
 
@@ -20,13 +54,31 @@ module.exports.newFile = async (project, fileName) => {
  * @param {String} folderName fileName
  */
 module.exports.deleteFolder = async (project, folderName) => {
+  if (!project) throw new AppError('project is required')
+  if (!folderName) throw new AppError('folder name is required')
 
+  const folderPath = path.join(PROJECT_STORAGE, project, folderName)
+  if (!(await exists(folderPath))) throw new AppError('file is not exist')
+
+  await removeItem(folderPath)
+
+  return 'done'
 }
 
 /**
  * @param {String} project projectName
- * @param {String} folderName fileName
+ * @param {String} folder folderName or path to folder e.x: /as/d/folderName
  */
-module.exports.newFolder = async (project, folderName) => {
+module.exports.newFolder = async (project, folder) => {
+  if (!project) throw new AppError('project is required')
+  if (!folder) throw new AppError('folder name is required')
 
+  const folderPath = path.join(PROJECT_STORAGE, project, folder)
+  const parentFolder = path.dirname(folderPath)
+
+  if (!(await exists(parentFolder))) throw new AppError('parent folder is not exist')
+  if (await exists(folderPath)) throw new AppError('folder is exist')
+  
+  await mkdir(folderPath)
+  return 'done'
 }
--- src/file-sys/route.js
+++ src/file-sys/route.js
@@ -1,5 +1,50 @@
 const route = require('express').Router()
+const controller = require('./controller')
 
 
+route.get('/remove-file', async (req, res) => {
+  const { project, file } = req.query
+  try {
+    const data = await controller.deleteFile(project, file)
+    res.status(200).json({data})
+  } catch (error) {
+    if (!error.isOperational) throw error
+    res.status(400).json({ message: error.message })
+  }
+})
+
+
+route.get('/remove-folder', async (req, res) => {
+  const { project, folder } = req.query
+  try {
+    const data = await controller.deleteFile(project, folder)
+    res.status(200).json({data})
+  } catch (error) {
+    if (!error.isOperational) throw error
+    res.status(400).json({ message: error.message })
+  }
+})
+
+route.get('/new-file', async (req, res) => {
+  const { project, file } = req.query
+  try {
+    const data = await controller.newFile(project, file)
+    res.status(200).json({data})
+  } catch (error) {
+    if (!error.isOperational) throw error
+    res.status(400).json({ message: error.message })
+  }
+})
+
+route.get('/new-folder', async (req, res) => {
+  const { project, folder } = req.query
+  try {
+    const data = await controller.newFolder(project, folder)
+    res.status(200).json({data})
+  } catch (error) {
+    if (!error.isOperational) throw error
+    res.status(400).json({ message: error.message })
+  }
+})
 
 module.exports = route
