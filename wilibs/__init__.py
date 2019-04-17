f43f697045e98afb984f8fe1a1680e6c793bee93
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2019-04-17 10:10:43 +0700

    update wilibs

--- wilibs/__init__.py
+++ wilibs/__init__.py
@@ -1,24 +1,26 @@
 import os
 import requests
 
-
-
 def login(username, password):
   root_url = os.environ.get('USER_RELATED_ROOT_URL', 'http://localhost:3000')
   url = root_url + '/login'
   payload = {
     "username": str(username),
-    "password": str(password)
+    "password": str(password),
+    "whoami": 'main-service'
   }
   r = requests.post(url, json=payload, verify=False)
   return r.json()
 
 
-def list_project():
+def list_project(token):
   root_url = os.environ.get('PROJECT_RELATED_ROOT_URL', 'http://localhost:3000')
   url = root_url +  '/project/list'
   payload = {}
-  r = requests.post(url, json=payload, verify=False)
+  headers = {
+    'Authorization': str(token)
+    }
+  r = requests.post(url, json=payload, verify=False, headers=headers)
   return r.json()
 
 
@@ -26,11 +28,11 @@ def list_well_of_project(idProject, start, limit, forward, match):
   root_url = os.environ.get('PROJECT_RELATED_ROOT_URL', 'http://localhost:3000')
   url = root_url +  '/project/well/list'
   payload = {
-    "idProject": str(idProject),
-    "start": str(start),
-    "limit": str(limit),
-    "forward": str(forward),
-    "match": str(match)
+    "idProject": idProject,
+    "start": start,
+    "limit": limit,
+    "forward": forward,
+    "match": match
   }
   r = requests.post(url, json=payload, verify=False)
   return r.json()
@@ -48,7 +50,7 @@ def get_curve_info(idReferenceCurve):
   root_url = os.environ.get('PROJECT_RELATED_ROOT_URL', 'http://localhost:3000')
   url = root_url +  '/project/well/reference-curve/info'
   payload = {
-    "idReferenceCurve": str(idReferenceCurve)
+    "idReferenceCurve": idReferenceCurve
   }
   r = requests.post(url, json=payload, verify=False)
   return r.json()
