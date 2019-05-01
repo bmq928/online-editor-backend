3c62cfb5478015433d3254a5bcca530ffb6fd0c4
thinhlv <thinhlevan2015@gmail.com>
2019-05-01 23:57:46 +0700

    update demo

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ project_obj.createWell(client.token, 43, name = "test_well", unit = "cm")
 project = client.getProjectById(43)
 list_well_in_project = project.getListWell()
 
-print(list_well_in_project)
+#print(list_well_in_project)
 
 #get well info (token, wellID)
 
@@ -45,31 +45,58 @@ well_info = well_obj.getWellFullInfo(client.token, 51)
 well_obj.editWellInfo(client.token, 51, name = "changed_name_test-well", topDepth = 50, bottomDepth = 60)
 
 #delete well (token, wellID)
+'''
+well_to_delete = client.getWellById(54)
+well_to_delete.deleteWell()
+'''
 
-well_obj.deleteWell(client.token, 51)
 
 """dataset demo"""
 
 #create dataset 
-"""loi code ?"""
- # list dataset
 
+w = client.getWellById(51)
+w.createDataset(name= '1234', unit = 'm', step = 0.1, bottom = 10, top = 1)
+
+# list dataset
+list_dataset = w.getListDataset()
+
+#edit dataset
+
+dataset_to_edit = client.getDatasetById(32)
+err = dataset_to_edit.editDatasetInfo(name = 'hello', datasetKey='new key', idWell='2')
+if err:
+    print(err)
+
+#delete dataset
 
 
 """curve demo"""
 
-# curve = client.getCurveById(99)
 
-# err = curve.editCurveInfo(unit = 'cm', name = 'Demo')
+#create curve
 
-# if err:
-#     print(err)
-# else:
-#     print(curve.getCurveInfo())
+dataset = client.getDatasetById(32)
+curve = dataset.createCurve('456', initValue = 10, unit = 'm')
 
+#list curve
+data = client.getDatasetById(32)
 
-dataset = client.getDatasetById(30)
+list_curve_in_dataset = data.getListCurve()
 
-curve = dataset.createCurve('new Curve check', initValue = 10)
+#update curve by file
 
+curve_to_edit = client.getCurveById(124)
+file = open('F:\Workspace\wi-python-backend\data.txt', 'rb')
+err = curve_to_edit.updateCurveDataByFile(file)
+if err:
+    print(err)
 
+#edit curve info
+err = curve.editCurveInfo(name = '789', initValue = 69, unit = 'cm', dulplicate = 1)
+if err:
+    print(err)
+
+#delete curve
+
+curve_to_edit.deleteCurve()
