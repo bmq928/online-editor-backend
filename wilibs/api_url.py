d5de8deb0bc3953620262de5d364c335c8596f63
BuiHoang <bdhoang.ht@gmail.com>
2019-05-30 15:09:53 +0700

    add functions

--- wilibs/api_url.py
+++ wilibs/api_url.py
@@ -1,2 +1,4 @@
-AUTH_API = 'http://admin.dev.i2g.cloud'
-ROOT_API = 'http://dev.i2g.cloud'
+# AUTH_API = 'http://admin.dev.i2g.cloud'
+# ROOT_API = 'http://dev.i2g.cloud'
+AUTH_API = 'https://users.i2g.cloud'
+ROOT_API = 'httpa://api-1.i2g.cloud'
