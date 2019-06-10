be1ec79f55de56a8a75ad90db87c53fa531aff15
BuiHoang <bdhoang.ht@gmail.com>
2019-06-10 09:51:23 +0700

    fix

--- wilibs/api_url.py
+++ wilibs/api_url.py
@@ -1,6 +1,6 @@
 import os
 
-AUTH_API = 'https://users.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'http://admin.dev.i2g.cloud'
-ROOT_API = 'https://api-1.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'http://dev.i2g.cloud'
+AUTH_API = 'https://users.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'https://users.i2g.cloud'
+ROOT_API = 'https://api-1.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'https://api-1.i2g.cloud'
 EXPORT_PATH = '/app/export' if os.environ["NODE_ENV"] == 'production' else '/app/export'
-DOWNLOAD_BASE_URL = 'https://python.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'http://python.dev.i2g.cloud'
+DOWNLOAD_BASE_URL = 'https://python.i2g.cloud' if os.environ["NODE_ENV"] == 'production' else 'https://python.i2g.cloud'
