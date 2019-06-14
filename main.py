40e87481fc893e62b65571a8ab94463d5206408b
unknown <sockyone16899@gmail.com>
2019-06-14 15:35:38 +0700

    converUnit function

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from wilibs.project import project_obj
 from wilibs.project.well import well_obj
 from wilibs.project.well.dataset import dataset_obj
 from wilibs.project.well.dataset.curve import curve_obj
+from wilibs.common import *
 import json
-
+from wilibs.common import convertUnit
 
 #login
-client = wilib.login("hoang","1")
+# client = wilib.login("hoang","1")
 
 # arrayCurve = client.findCurveByName("array_curve","source","g_1x","demo_edit_curve")
 # textCurve = client.findCurveByName("text_curve","source","g_1x","demo_edit_curve")
 # singleCurve = client.findCurveByName("bs","source","g_1x","demo_edit_curve")
 
-well = client.getWellById(31)
+# well = client.getWellById(31)
 
-well.limitWell(10,1300,'m')
+# well.limitWell(10,1300,'m')
+
+print(convertUnit(1000,'.0005 M','.01 M'))
