163a2c3029bb194557e2253a671689dbe4537e44
BuiHoang <bdhoang.ht@gmail.com>
2019-07-25 12:15:42 +0700

    fix

--- wilibs/api_url.py
+++ wilibs/api_url.py
@@ -1,6 +1,6 @@
 import os
 
-AUTH_API = 'https://users.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://admin.dev.i2g.cloud'
-ROOT_API = 'https://api-1.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://dev.i2g.cloud'
+AUTH_API = 'https://users.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://admin.dev.i2g.cloud' if os.environ["PYTHON_ENV"] == 'dev' else os.environ["USER_RELATED_ROOT_URL"]
+ROOT_API = 'https://api-1.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://dev.i2g.cloud' if os.environ["PYTHON_ENV"] == 'dev' else os.environ["PROJECT_RELATED_ROOT_URL"]
 EXPORT_PATH = '/app/export' if os.environ["PYTHON_ENV"] == 'production' else '/app/export'
-DOWNLOAD_BASE_URL = 'https://python.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://python.dev.i2g.cloud'
+DOWNLOAD_BASE_URL = 'https://python.i2g.cloud' if os.environ["PYTHON_ENV"] == 'production' else 'http://python.dev.i2g.cloud' if os.environ["PYTHON_ENV"] == 'dev' else os.environ["PYTHON_DOWNLOAD_BASE_URL"]
